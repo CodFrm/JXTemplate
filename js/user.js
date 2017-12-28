c511bd6b76b04df2a99a2af62d47460d9e586c4a
CodFrm <code.farmer@qq.com>
2017-12-28 11:43:45 +0800

    frist

---
+++ js/user.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(document).on('mousemove','.tab-item',function(e){
+        //bar 特效
+        $('#tab-bar').css({left:$(this).position().left,width:$(this).width()});
+    });
+    $(document).on('mouseout','.tab-item',function(e){
+        //bar 特效
+        $('#tab-bar').css({left:$('.tab-item.active').position().left,width:$('.tab-item.active').width()});
+    });
+    $('#tab-bar').css({left:$('.tab-item.active').position().left,width:$('.tab-item.active').width()});
+    // $('.tab-item.active').css({color:'#22b9e7'});
+})
